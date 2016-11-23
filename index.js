a59fa0b680978ca0d424a3b0d358c019c9c1a5e7
Marc Bachmann <marc.brookman@gmail.com>
2016-11-23 02:08:33 +0100

    chore(package): update uuid to version 3.0.0

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var uuid = require('node-uuid');
+var uuid = require('uuid');
 
 var regexps = {
 	uuid: /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i,
